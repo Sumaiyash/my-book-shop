0fd272bce7fc774d0ed19e2a9b534f5aa361834a
Sumaiyashe <sumaiyashompa137@gmail.com>
2022-03-26 03:25:05 +0600

    finally coded is running

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h2>here is my first website</h2>
+      <p>finally i do it</p>
     </div>
   );
 }
